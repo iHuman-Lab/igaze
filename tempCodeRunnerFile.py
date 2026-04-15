8bb6bbfbf50c17c34ccd3104e8979cf12c6b3acd
Hemanth Manjunatha <hemanthm2277@gmail.com>
2026-04-15 11:42:07 -0500

    Refactored code

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-with skip_run("run", "fixation_extraction") as check, check():
-    raw_fixations, fixation_summary = extract_fixations_from_config(config_path)
-    print("Fixation summary:")
-    print(fixation_summary)
